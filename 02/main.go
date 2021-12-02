021bdae08311b155cc0ffe1a710b117dd19ece88
Rob Denman <rob.denman@datasite.com>
2021-12-02 08:53:47 -0600

    day 2

---
+++ 02/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type command struct {
+	dir string
+	val int
+}
+
+func main() {
+	sol1 := solution1()
+	sol2 := solution2()
+
+	fmt.Printf("Solution #1: %d\n", sol1)
+	fmt.Printf("Solution #2: %d\n", sol2)
+}
+
+func solution1() int {
+	data := readInputData()
+	
+	hor, dep := 0, 0
+	for _, c := range data {
+		switch c.dir {
+		case "forward":
+			hor += c.val
+		case "down":
+			dep += c.val
+		case "up":
+			dep -= c.val
+		}
+	}
+
+	return hor * dep
+}
+
+func solution2() int {
+	data := readInputData()
+
+	hor, dep, aim := 0, 0, 0
+	for _, c := range data {
+		switch c.dir {
+		case "forward":
+			hor += c.val
+			dep += aim * c.val
+		case "down":
+			aim += c.val
+		case "up":
+			aim -= c.val
+		}
+	}
+
+	return hor * dep
+}
+
+func readInputData() []command {
+	input, err := os.ReadFile("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows := strings.Split(string(input), "\n")
+	data := make([]command, len(rows))
+	for i, r := range rows {
+		kv := strings.Split(r, " ")
+		num, err := strconv.Atoi(kv[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		data[i] = command{kv[0], num}
+	}
+
+	return data
+}
